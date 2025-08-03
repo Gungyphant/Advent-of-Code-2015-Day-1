7f9e99487854b8862cc404aa6333c60dff8c82d4
Gungyphant <gungyphant@gmail.com>
2025-08-03 17:35:28 +0100

    Created working solution

--- Program.cs
+++ Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace AOC2015Day1
+{
+    class Program
+    {
+        static string PartOne(string data)
+        {
+            int floor = 0;
+            foreach (char c in data)
+            {
+                if (c == '(')
+                {
+                    floor++;
+                }
+                else if (c == ')')
+                {
+                    floor--;
+                }
+            }
+            return Convert.ToString(floor);
+        }
+        static string PartTwo(string data)
+        {
+            int floor = 0;
+            int instructions = 0;
+            foreach (char c in data)
+            {
+                instructions++;
+                if (c == '(')
+                {
+                    floor++;
+                }
+                else if (c == ')')
+                {
+                    floor--;
+                }
+                if (floor < 0)
+                {
+                    break;
+                }
+            }
+            return Convert.ToString(instructions);
+        }
+        static void Main()
+        {
+            string file = File.ReadAllText(@"../../../input.txt");
+            Console.WriteLine(PartOne(file));
+            Console.WriteLine(PartTwo(file));
+        }
+    }
+}
